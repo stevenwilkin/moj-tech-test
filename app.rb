f29766fd570f831fc46352c5cb069766a2bffd36
Steven Wilkin <stevenwilkin@gmail.com>
2016-07-31 22:20:22 +0100

    Return anagrams of single word requests

--- app.rb
+++ app.rb
@@ -1,9 +1,13 @@
 require 'sinatra'
 require 'json'
 
+require_relative 'lib/anagrams'
+
 class App < Sinatra::Base
   get '/:word' do
     content_type :json
-    {}.to_json
+    {
+      params[:word] => Anagrams.for(params[:word])
+    }.to_json
   end
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe App do
 
   describe 'GET /anagram' do
     let(:word) { 'crepitus' }
+    let(:anagrams) { %w(cuprites pictures piecrust) }
 
     before do
       get "/#{word}"
@@ -24,5 +25,12 @@ RSpec.describe App do
     specify { expect(last_response).to be_ok  }
     specify { expect(last_response.headers['Content-Type']).to eq('application/json') }
     specify { expect { json_response }.to_not raise_error }
+    specify { expect(json_response[word]).to eq(anagrams) }
+
+    context 'when word is unknown' do
+      let(:word) { 'sdfwehrtgegfg' }
+
+      specify { expect(json_response[word]).to eq([]) }
+    end
   end
 end
