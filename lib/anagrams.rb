5b19c6a4e3a93c0d1f019ef4f05351ef9db8246c
Steven Wilkin <stevenwilkin@gmail.com>
2016-07-31 21:31:01 +0100

    Put all words in a bucket according to their hashed value

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -3,5 +3,36 @@ class Anagrams
     def hash_for(word)
       word.chars.sort.join
     end
+
+    def words
+      %w(
+        crepitus
+        cuprites
+        pictures
+        piecrust
+        paste
+        pates
+        peats
+        septa
+        spate
+        tapes
+        tepas
+      )
+    end
+
+    def buckets
+      @buckets ||= begin
+        words.reduce({}) do |results, word|
+          hash = hash_for(word)
+          results[hash] ||= []
+          results[hash] << word
+          results
+        end
+      end
+    end
+
+    def bucket_for(hash)
+      buckets[hash]
+    end
   end
 end
--- spec/lib/anagrams_spec.rb
+++ spec/lib/anagrams_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe Anagrams do
 
     it { is_expected.to eq(hashed) }
   end
+
+  describe '.bucket_for' do
+    let(:hash) { 'ceiprstu' }
+    let(:bucket) { %w(crepitus cuprites pictures piecrust) }
+    subject { Anagrams.bucket_for(hash) }
+
+    it { is_expected.to eq(bucket) }
+  end
 end
